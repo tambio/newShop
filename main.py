9035cc349061ea7789a57f17483a74e436e0f6ed
tampi <ormanamir3@gmail.com>
2024-01-16 20:55:36 +0600

    test_format_4_batyr

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
-# app.py
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 import psycopg2
 from config import db_name, user, password, host
 
 app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return render_template('index.html')
 
 def fetch_data(query):
     with psycopg2.connect(database=db_name, user=user, host=host, password=password) as conn:
@@ -15,60 +10,17 @@ def fetch_data(query):
             curs.execute(query)
             result = curs.fetchall()
             column_names = [desc[0] for desc in curs.description]
-            return render_template('show_data.html', column_names=column_names, data=result)
+            return {'column_names': column_names, 'data': result}
 
-@app.route('/brands')
-def show_brand():
-    return fetch_data("SELECT * FROM brands")
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-@app.route('/categories')
-def show_categories():
-    return fetch_data("SELECT * FROM categories")
-
-@app.route('/products')
-def show_products():
-    return fetch_data("SELECT * FROM products")
-
-@app.route('/warehouses')
-def show_warehouses():
-    return fetch_data("SELECT * FROM warehouse")
-
-@app.route('/orders')
-def show_orders():
-    return fetch_data("SELECT * FROM orders")
-
-@app.route('/ordered_products')
-def show_ordered_products():
-    return fetch_data("SELECT * FROM ordered_products")
-
-@app.route('/customers')
-def show_customers():
-    return fetch_data("SELECT * FROM customers")
-
-@app.route('/product_availability')
-def show_inventory():
-    return fetch_data("SELECT * FROM product_availability")
-
-@app.route('/shopping_cart')
-def show_cart_products():
-    return fetch_data("SELECT * FROM shopping_cart")
-
-@app.route('/reviews')
-def show_feedbacks():
-    return fetch_data("SELECT * FROM reviews")
-
-@app.route('/information')
-def show_info():
-    return fetch_data("""
-        SELECT o.order_id, o.order_date, c.customer_name, d.delivery_date, d.delivery_address
-        FROM orders o
-        JOIN customers c ON o.customer_id = c.customer_id
-        LEFT JOIN delivery d ON o.order_id = d.order_id;
-    """)
-
-@app.route('/deliveries')
-def show_deliveries():
-    return fetch_data("SELECT * FROM delivery")
+@app.route('/query')
+def handle_query():
+    query = request.args.get('query', '')
+    data = fetch_data(query)
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
